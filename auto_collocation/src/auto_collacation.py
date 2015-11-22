0e6106e3c54947410d5d270612dc731979cdefd6
shenh10 <thushenhan@gmail.com>
2015-11-22 20:59:35 +0800

    finish frequency approach

--- auto_collocation/src/auto_collacation.py
+++ auto_collocation/src/auto_collacation.py
@@ -4,6 +4,8 @@ import codecs
 import nltk
 from os import walk
 import pickle
+import matplotlib.pyplot as plt
+import numpy as np
 
 ''' \u4e00-\u9fff simplified chinese; 
     \uff10-\uff19 gbk numbers;
@@ -62,10 +64,12 @@ def load_data(mypath):
     word_tagged_set = []
     wordpair_set = []
     combinations = []
-    combination = []
-    tagged_article = []
     for i, filename in enumerate(f):
+        if i == 10:
+            break
         with codecs.open(mypath+'/'+filename, 'r',encoding='gbk') as fd:
+            tagged_article = []
+            combination = []
             print "process file %d"%i
             for line in fd:
                 for t in line.split():
@@ -100,19 +104,53 @@ def process_data(data):
  #           for i in text.split(' ') if i]
   #          wordset.append(text.split(' ').)
     return word_tagged_set
+def statistics(word_tagged_set, combinations):
+    tag_total_fd = nltk.ConditionalFreqDist( (tag, word) for (word, tag) in word_tagged_set) 
+    print combinations
+    tag_combination_fd = nltk.ConditionalFreqDist( (tag, word) for (word, tag) in combinations)
+    counts = [ ( tag_total_fd[index].N(), index) for index in tag_total_fd.conditions()]
+    counts += [ (tag_total_fd[index].N(), index) for index in tag_combination_fd.conditions()]
+    counts = sorted(counts)
+    tags = [('%s'%index).encode('utf-8') for (_, index) in counts]
+    fig, ax = plt.subplots()
+    width = 0.9
+    ind = np.arange(len(tags))
+    rects1 = ax.bar(ind , [ _ for ( _, index) in counts] , width, color='r',label='Number of Words in Each Tag')
+    plt.xticks(ind + width, rotation=45)
+    ax.set_xticklabels(tags)
+    plt.title('Word Distribution')
+    plt.xlabel('Tags')
+    plt.ylabel('Number of Words')
+    plt.show()
 
+def frequency(word_tagged_set, wordpair_set, combinations):
+    #tags = set([tag for (word, tag ) in word_tagged_set]+ [tag for (word, tag) in combinations])
+    common_set = set(['U','P','R','M', 'Q', 'T'])
+    wordpair_fd = nltk.FreqDist( (a[0],b[0], a[1], b[1]) for (a, b) in wordpair_set) 
+    tag_combination_fd = nltk.FreqDist( (word, tag) for (word, tag) in combinations)
+    # Naively rank all biagram pairs
+    naive_counts = [ ' '.join(list(collocation)+['%d'%count]) for (collocation ,count) in  wordpair_fd.most_common(100)]
+    # Filter those collocation with less sense
+    filter_counts = [ ' '.join(list(collocation)+['%d'%count]) for (collocation ,count) in  wordpair_fd.most_common() if (( collocation[2] not in common_set) and ( collocation[3] not in common_set)) ]
+    # count bigram pair frequency by tag type
+    wordpair_conditional_fd = nltk.ConditionalFreqDist( (' '.join([ a[1],b[1] ]), ' '.join([ a[0],b[0] ])) for (a, b) in wordpair_set )
+    category_rank = sorted([ (wordpair_conditional_fd[index].N(), index) for index in wordpair_conditional_fd.conditions() ], reverse=True)
+    # Get most frequent meaningful collocation by type
+    categoryList = [ ':\n'.join([ st, '\n'.join([ '%s %d'%(word, count)  for (word, count) in wordpair_conditional_fd[st].most_common(10) ]) ]) for (i, st) in category_rank[0:10]  ]
+    #print '\n'.join(categoryList).encode('utf-8')
+    #print '\n'.join( [ '%d %s'%(i, st) for (i, st) in category_rank])
+    #print '\n'.join(filter_counts).encode('utf-8')
+     
 if __name__ == '__main__':
     input_path = '../data/input'
     preprocessed_path = '../data/output/processed_data.pkl'
-    word_tagged_set, combinations, wordpair_set = load_data(input_path)
+    #word_tagged_set, combinations, wordpair_set = load_data(input_path)
     print 'Write to module file %s'%preprocessed_path
-    saveToFile((word_tagged_set, combinations,  wordpair_set), preprocessed_path)
+    #saveToFile((word_tagged_set, combinations,  wordpair_set), preprocessed_path)
     print 'Load from module file %s'%preprocessed_path
     (word_tagged_set,combinations, wordpair_set) = loadFromFile(preprocessed_path)
-    print '\n\n\nPrint all words...'
-    #printTupleList(word_tagged_set)
-    print '\n\n\nPrint all combinations words...'
-    #printTupleList(combinations)
+    #statistics(word_tagged_set, combinations)
+    frequency(word_tagged_set,wordpair_set, combinations)
     print '\n\n\nPrint all bigram words...'
     #printBigramTupleList( wordpair_set )
     #print wordset
